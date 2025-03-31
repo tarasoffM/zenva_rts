e8391cafc552ee4bb4d41e05eab48909efe0d551
Matt Tarasoff <tarasoff.matt@gmail.com>
2025-03-31 12:19:23 -0400

    added some attack logic to the unit_base

--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -21,12 +21,13 @@ var attack_target : Unit
 
 @onready var agent : NavigationAgent2D = $NavigationAgent2D
 
-func _ready():
-	set_move_to_target(Vector2.ZERO)
+
 
 func _process (delta):
-	if not agent.is_target_reached():
+	if not agent.is_navigation_finished():
 		_move(delta)
+		
+	_target_check()
 	
 func _move (delta):
 	var move_pos = agent.get_next_path_position()
@@ -36,20 +37,43 @@ func _move (delta):
 	translate(movement)
 	
 func _target_check ():
-	pass
+	if attack_target == null:
+		return
+		
+	var dist = global_position.distance_to(attack_target.global_position)
+	
+	if dist <= attack_range:
+		agent.target_position = global_position
+		_try_attack_target()
+	else:
+		agent.target_position = attack_target.global_position
 	
 func _try_attack_target ():
-	pass
+	var time = Time.get_unix_time_from_system()
+	
+	if time - last_attack_time < attack_rate:
+		return
+		
+	last_attack_time = time
+	attack_target.take_damage(attack_damage)
 	
 func set_move_to_target (target: Vector2):
 	agent.target_position = target
 	attack_target = null
 	
 func set_attack_target (target: Unit):
-	pass
+	if target.team == team:
+		return
+	
+	attack_target = target
 	
 func take_damage (amount : int):
-	pass
+	cur_hp -= amount
+	OnTakeDamage.emit(cur_hp)
+	
+	if cur_hp <= 0:
+		_die()
 	
 func _die ():
-	pass
+	OnDie.emit(self)
+	queue_free()
